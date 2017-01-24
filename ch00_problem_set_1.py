4a514f36a538e072a3fe83d805512d6a6102c2db
bernhardtjj <jbernhardt@fwparker.org>
2017-01-23 19:12:31 -0600

    Done the homework

--- ch00_problem_set_1.py
+++ ch00_problem_set_1.py
@@ -1,12 +1,12 @@
 #SECTION 1 - MATH OPERATORS AND VARIABLES (20PTS TOTAL)
-
+from math import *
 #PROBLEM 1 (From Math Class to Code - 2pts)
-
-# Print the answer to the math question:
-# 3(60x^2 + 3x/9) + 2x - 4/3(x) - sqrt(x)
-# where x = 12.83
 x = 12.83
-your_answer = "Enter the equation"
+# Print the answer to the math question:
+your_answer = 3 * (60*x**2 + 3*x/9) + 2*x - 4/3*(x) - sqrt(x)
+# where x = 12.83
+
+#your_answer = "Enter the equation"
 print(your_answer)
 
 #PROBLEM 2 (Set your alarm - 3pts)
@@ -16,13 +16,23 @@ print(your_answer)
 # At what time will the alarm go off? Write a program that prints the answer.
 # Hint: for the best solution, you will need the modulo operator.
 
-
+time = 13.00
+time += 728.00
+newtime = (time % 24)
+if newtime > 12.00:
+    print("{:0.2f}".format(newtime - 12.00), "pm")
+else:
+    print("{:0.2f}".format(newtime), "am")
 
 #PROBLEM 3 (Wholesale Books - 3pts)
 #The cover price of a book is $27.95, but bookstores get a 50 percent discount.
 #Shipping costs $4 for the first copy and 75 cents for each additional copy.
 # Calculate the total wholesale costs for 68 copies to the nearest penny.
-
+cover_price = 27.95
+discount = 0.5
+copies = 68
+shipping = 0.75*copies + 4
+print("${:0.2f}".format(cover_price*copies*discount+shipping))
 
 #PROBLEM 4 (Dining Room Chairs - 3pts)
 # You purchase eight chairs for your dining room.
@@ -33,12 +43,16 @@ chair_price = 189.99
 tax_percent = 0.095
 units = 8
 
+print(round(units*chair_price*tax_percent,2))
+
 #PROBLEM 5 (Area of Circle - 3pts)
 # Write code that can compute the area of circle.
 # Create variables for radius and pi (3.14159)
 # The formula, in case you do not know, is radius times radius times pi.
 # Print the outcome of your program as follows:
 # “The surface area of a circle with radius ... is ...”
+radius = 3
+print("The surface area of a circle with radius", radius, "is", pi*radius**2)
 
 #PROBLEM 6 (Coin counter - 4pts)
 # Write code that classifies a given amount of money (which you store in a variable named count),
@@ -50,6 +64,16 @@ units = 8
 # and so on for nickels and pennies.
 # The result is that you express the amount as the minimum number of coins needed.
 
+count = 63.34
+print("dollars:", count // 1.00)
+print("quartars:", count % 1.00 // 0.25)
+print("dimes:", (count % 1.00) % 0.25 // 0.10)
+print("nickels:", ((count % 1.00) % 0.25) % 0.10 // 0.05)
+print("pennies:", (((count % 1.00) % 0.25) % 0.10) % 0.05 // 0.01)
+# I found a float precision error in python! make count 17.65 and print (count % 1.00) % 0.25) % 0.10) % 0.05 and you will see what I mean! it isn't 0.05...
+
+
+
 #PROBLEM 7 (Variable Swap - 2pts)
 # Can you think of a way to swap the values of two variables that does not
 # need a third variable as a temporary storage?
@@ -60,6 +84,8 @@ units = 8
 a = 17
 b = 23
 print( "a =", a, "and b =", b)
-a += b # this is the first line to help you out
+a = a+b # this is the first line to help you out
+b = a-b
+a = a-b
 # add two more lines of code here to cause swapping of a and b
 print( "a =", a, "and b =", b)
